98326eaa40e0cbd640f876e6fa92ace0bf73fd6c
acerola <ffxivacerola@gmail.com>
2024-07-20 20:34:06 -0500

    placeholder to test mch

--- fflogs_rotation/tests/test_machinist.py
+++ fflogs_rotation/tests/test_machinist.py
@@ -1,2 +1,16 @@
 from fflogs_rotation.machinist import MachinistActions
 
+# https://www.fflogs.com/reports/bLcB1DYCKxq8tdf6#fight=6
+# 
+# Dexterity
+# 4339
+# Vitality
+# 4387
+# Critical Hit
+# 2002
+# Direct Hit Rate
+# 2099
+# Determination
+# 2534
+# Skill Speed
+# 420
